8c63cd2fb7b8b921526586031a6c59cb9c7fde20
Kashish <kashishbhagat45@gmail.com>
2026-09-20 13:01:15 +0530

    Update train.py
    train DecisionTree Classifier

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-
-model=LogisticRegression()
+from sklearn.tree import DecisionTreeClassifier
+model=DecisionTreeClassifier()
 
 model.fit(X_train,y_train)
